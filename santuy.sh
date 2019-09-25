67a42b0046f20aae7a074b61d3ee25a5cbd2cece
MltrCyber <52839806+MltrCyber@users.noreply.github.com>
2019-09-25 20:14:06 +0700

    Add files via upload

---
+++ santuy.sh
@@ -0,0 +1,248 @@
+#!/bin/sh
+pkg update -y
+pkg upgrade -y
+pkg install ruby -y
+pkg install git -y
+pkg install figlet -y
+pkg install python python2 -y
+pkg install toilet
+pkg install gem -y
+gem install lolcat
+clear
+toilet -f big -F gay "welcome"
+sleep 3
+figlet "LOADING"|lolcat
+sleep 1
+echo "Please WAIT"|lolcat
+sleep 3
+echo "starting tools    ";
+sleep 0.1;
+clear
+echo "Starting tools.   ";
+sleep 0.1;
+clear
+echo "sTarting tools..  ";
+sleep 0.1;
+clear
+echo "stArting tools... ";
+sleep 0.1;
+clear
+echo "staRting tools    ";
+sleep 0.1;
+clear
+echo "starTing tools.   ";
+sleep 0.1;
+clear
+echo "startIng tools..  ";
+sleep 0.1;
+clear
+echo "startiNg tools... ";
+sleep 0.1;
+clear
+echo "startinG tools    ";
+sleep 0.1;
+clear
+echo "starting Tools.   ";
+sleep 0.1;
+clear
+echo "starting tOols..  ";
+sleep 0.1;
+clear
+echo "starting toOls... ";
+sleep 0.1;
+clear
+echo "starting tooLs    ";
+sleep 0.1;
+clear
+echo "starting toolS.   ";
+sleep 0.1;
+clear
+echo "starting tools..  ";
+sleep 0.1;
+clear
+echo "Starting tools... ";
+sleep 0.1;
+clear
+echo "sTarting tools    ";
+sleep 0.1;
+clear
+echo "stArting tools.   ";
+sleep 0.1;
+clear
+echo "staRting tools..  ";
+sleep 0.1;
+clear
+echo "starTing tools... ";
+sleep 0.1;
+clear
+echo "startIng tools    ";
+sleep 0.1;
+clear
+echo "startiNg tools.   ";
+sleep 0.1;
+clear
+echo "startinG tools..  ";
+sleep 0.1;
+clear
+echo "starting Tools... ";
+sleep 0.1;
+clear
+echo "starting tOols    ";
+sleep 0.1;
+clear
+echo "starting toOls.   ";
+sleep 0.1;
+clear
+echo "starting tooLs..  ";
+sleep 0.1;
+clear
+echo "starting toolS... ";
+sleep 0.1;
+clear
+echo "starting tools    ";
+sleep 0.1;
+clear
+echo "Starting tools.   ";
+sleep 0.1;
+clear
+echo "sTarting tools... ";
+sleep 0.1;
+clear
+echo "stArting tools    ";
+sleep 0.1;
+clear
+echo "staRting tools.   ";
+sleep 0.1;
+clear
+echo "starTing tools..  ";
+sleep 0.1;
+clear
+echo "startIng tools... ";
+sleep 0.1;
+clear
+echo "startiNg tools    ";
+sleep 0.1;
+clear
+echo "startinG tools.   ";
+sleep 0.1;
+clear
+echo "starting Tools..  ";
+sleep 0.1;
+clear
+echo "starting tOols... ";
+sleep 0.1;
+clear
+echo "starting toOls    ";
+sleep 0.1;
+clear
+echo "starting tooLs.   ";
+sleep 0.1;
+clear
+echo "starting toolS..  ";
+sleep 0.1;
+clear
+echo "     #########################################"|lolcat
+echo "                       _ _ "|lolcat
+echo "                   o/ \ili/ \o "|lolcat
+echo "                      (o o) "|lolcat
+echo "                ooO--(/___\)--Ooo"|lolcat
+echo "               (===Mr.DonXont===)"|lolcat
+echo "Author    : Mr.DonXont"|lolcat
+echo "My Team   : F0URSD34TH T34M"|lolcat
+echo "My Team   : ANONYMOUS CYBER MUSLIM INDONESIA"|lolcat
+echo "Thanks To : Mr.13DL,Mr.S3P0NG#013"|lolcat
+echo "            Umar Ganss,ACMI||ZhaBhaY,H3RM4W4N KUN"|lolcat
+echo "            Mr.OxyGeini7,Mr.Z4MU3L,Raka Rohman"|lolcat
+echo "            Mr.R1Z4L,1NS4Y N3T,Mr.ProjEcT Er0R"|lolcat
+echo
+echo "     #########################################"|lolcat
+echo
+echo "       <<<<<<<<<<<<<<<<<=>>>>>>>>>>>>>>>>>"|lolcat
+echo "      [-] WELCOME TO MY TOOLS INSTALLER [-]"|lolcat
+echo "       <<<<<<<<<<<<<<<<<=>>>>>>>>>>>>>>>>>"|lolcat
+echo
+echo "================================"|lolcat
+echo "     -= Hack WhatsApp =-      "|lolcat
+echo "================================"|lolcat
+echo "1.) whats hack"|lolcat
+echo "================================"|lolcat
+echo "    -= Kumpulan--Tools =-     "|lolcat
+echo "================================"|lolcat
+echo "2.) DonXont Tools"|lolcat
+echo "3.) TUANB4DUT"|lolcat
+echo "================================"|lolcat
+echo "     -= SCRIPT DEFACE =-      "|lolcat
+echo "================================"|lolcat
+echo "4.) Ubaii"|lolcat
+echo "5.) LITESCRIPT"|lolcat
+echo "================================"|lolcat
+echo $green
+read -p "Mr. DonXont # Pilih Angka = " BDL;
+
+if [ $BDL = 1 ] || [ $BDL = 1 ]
+then
+clear
+toilet -f big -F gay "whatshack"
+sleep 3
+pkg update && pkg upgrade
+pkg install git
+pkg install curl
+git clone https://github.com/AndriGanz/whatshack
+cd whatshack
+sh  whatshack.sh
+fi
+
+if [ $BDL = 2 ] || [ $BDL = 2 ]
+then
+clear
+toilet -f big -F gay "DonXont"
+sleep 3
+pkg update -y
+pkg upgrade -y
+pkg install git -y
+git clone https://github.com/DonXont/WFTD
+cd WFTD
+sh DonXont.sh
+fi
+
+if [ $BDL = 3 ] || [ $BDL = 3 ]
+then
+clear
+toilet -f big -F gay "TUANB4DUT"
+sleep 3
+apt update && apt upgrade
+pkg install python python2 vim figlet curl
+pkg install php
+pip2 install lolcat
+pkg install git
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+cd TOOLSINSTALLERv4
+chmod 777 TUANB4DUT..sh
+./TUANB4DUT..sh
+fi
+
+if [ $BDL = 4 ] || [ $BDL = 4 ]
+then
+clear
+toilet -f big -F gay "Ubaii"
+sleep 3
+apt update && apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/Ubaii/script-deface-creator
+cd script-deface-creator
+chmod +x create.py
+python2 create.py
+fi
+
+if [ $BDL = 5 ] || [ $BDL = 5 ]
+then
+clear
+toilet -f big -F gay "LITESCRIPT"
+sleep 3
+pkg install python2
+pkg install git
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+fi
